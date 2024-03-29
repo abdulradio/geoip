5dfc529f26f08809f5e5d40d80d7b195bde6e26f
abdulradio <abdul.nalim@yandex.ru>
2024-03-29 16:08:40 +0300

    update asn.sh

--- asn.sh
+++ asn.sh
@@ -25,12 +25,44 @@ done <${input}
 url_ru="https://stat.ripe.net/data/country-resource-list/data.json?resource=RU"
 url_by="https://stat.ripe.net/data/country-resource-list/data.json?resource=BY"
 
-echo "Generating RU CIDR list..."
-curl -sL ${url_ru} -o ./tmp/ip_RU.txt \
-      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
-    jq --raw-output '.data.resources.ipv4[]' ./tmp/ip_RU.txt | sort -u >>ripe/ip_RU.lst
+ripe_ip=$(curl -s "$url_ru" | jq -r '.data.resources.ipv4[]')
 
-echo "Generating BY CIDR list..."
-curl -sL ${url_by} -o ./tmp/ip_BY.txt \
-      -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
-    jq --raw-output '.data.resources.ipv4[]' ./tmp/ip_BY.txt | sort -u >>ripe/ip_BY.lst
+for record in $ripe_ip; do
+  if [[ "$record" == *-* ]]; then
+    ips=($(echo "$record" | tr '-' ' '))
+    ipaddr=$(sipcalc -s "${ips[0]}"-"${ips[1]}" | awk '/Network/ {print $3}')
+  else
+    ipaddr=$record
+  fi
+  networks+=("$ipaddr")
+done
+
+aggregate_prefixes() {
+  sipcalc -i 4 "${networks[@]}" | grep -E 'Network' | awk '{print $3}'
+}
+
+aggregate_prefixes | while read -r line; do
+  number_of_ips=$((number_of_ips + $(sipcalc -i 4 "$line" | grep -E 'Hosts.*[0-9]+' | awk '{print $2}')))
+  echo "$line"
+done > ripe/ip_RU.lst
+
+ripe_ip=$(curl -s "$url_by" | jq -r '.data.resources.ipv4[]')
+
+for record in $ripe_ip; do
+  if [[ "$record" == *-* ]]; then
+    ips=($(echo "$record" | tr '-' ' '))
+    ipaddr=$(sipcalc -s "${ips[0]}"-"${ips[1]}" | awk '/Network/ {print $3}')
+  else
+    ipaddr=$record
+  fi
+  networks+=("$ipaddr")
+done
+
+aggregate_prefixes() {
+  sipcalc -i 4 "${networks[@]}" | grep -E 'Network' | awk '{print $3}'
+}
+
+aggregate_prefixes | while read -r line; do
+  number_of_ips=$((number_of_ips + $(sipcalc -i 4 "$line" | grep -E 'Hosts.*[0-9]+' | awk '{print $2}')))
+  echo "$line"
+done > ripe/ip_BY.lst
